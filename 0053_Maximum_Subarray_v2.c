c48cd95720c637b30cf7b9e23569d379162ba98d
Amanda Tsai <dingyupei24@gmail.com>
2021-06-13 00:20:41 +0800

    File Arrangement

--- 0053_Maximum_Subarray_v2.c
+++
@@ -1,9 +0,0 @@
-int maxSubArray(int* nums, int numsSize){
-    int max = nums[0];
-    int local = nums[0];
-    for (int i = 1; i < numsSize; i++) {
-        local = (local > 0)? local + nums[i]: nums[i];
-        max = (max < local)? local: max;
-    }
-    return max;
-}
--- 0191_Number_of_1_Bits.c
+++
@@ -1,9 +0,0 @@
-int hammingWeight(uint32_t n) {
-    int i = 0;
-    int sum = 0;
-    for(i=0;i<32;i++){
-        sum += (n & 1);
-        n >>= 1;
-    }
-    return sum;
-}
